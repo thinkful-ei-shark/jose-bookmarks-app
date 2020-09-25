40a27ef1cc8d0ea2f4fd5ea650db9aad74bdc551
Joseguera <joseguera@uclaextension.edu>
2020-09-25 12:08:10 -0700

    Updated render function templates for initial page load and create new bookmark screen

--- src/index.js
+++ src/index.js
@@ -1,18 +1,83 @@
+/////////////////////////////////////////////////////////
+//////////////////// STORE DATABASE /////////////////////
+///////////////////////////////////////////////////////// 
+
 const store = {
-    items: [
-        { id: cuid(), name: 'Soccer News', url: 'https://www.goal.com/en-us', desc: 'Soccer news from around the world', rating: 4, expanded: true, editable: false },
-        { id: cuid(), name: 'Meatball Recipe', url: 'https://www.thekitchn.com/how-to-make-meatballs-cooking-lessons-from-the-kitchn-108048', desc: 'Easiest meatball recipe', rating: 3, expanded: true, editable: false },
-        { id: cuid(), name: 'Butthole Surfers - Who Was in My Room Last Night', url: 'https://www.youtube.com/watch?v=CNAkbbKycCM', desc: 'Pretty good song', rating: 2, expanded: false, editable: false }
+    bookmarks: [
+        { id: cuid(), title: 'Soccer News', rating: 3, url: 'https://www.goal.com/en-us', desc: 'Soccer news from around the world', expanded: true, editable: false },
+        { id: cuid(), title: 'Meatball Recipe', rating: 5, url: 'https://www.thekitchn.com/how-to-make-meatballs-cooking-lessons-from-the-kitchn-108048', desc: 'Easiest meatball recipe', expanded: true, editable: false },
+        { id: cuid(), title: 'Butthole Surfers - Who Was in My Room Last Night', rating: 4, url: 'https://www.youtube.com/watch?v=CNAkbbKycCM', desc: 'Soccer news from around the world', expanded: true, editable: false }
     ],
     hideExpandedItems: false,
-    hideEditOptions: false
+    hideEditOptions: false,
+    adding: false,
+    error: null,
+    filter: 0
 };
 
 /////////////////////////////////////////////////////////
 //////////////////// RENDER FUNCTIONS ///////////////////
 ///////////////////////////////////////////////////////// 
 
-  // STEP 1.B.
+
+//.:*~*:._.:*~*:._ STEP 1.C. _.:*~*:._.:*~*:.//
+
+// Function that returns HTML based on a switch case
+
+const generateStartTemplate = function() {
+    return `
+    <h1>Bookmarks List</h1>
+        <h2>Welcome fellow bookmarker!</h2>
+        <p class='description'>
+            This app will enable you to store your favorite
+            websites and organize by a rating (1 - 5) form
+            most favorite to "I can't believe I was into this band".
+        </p>
+        <p>Have fun and happy bookmarking 📚</p>
+        <form id='bookmark start js-bookmark-start'>
+            <button type="submit" >New Bookmark +</button>
+            <label for="bookmarks">Sort by:</label>
+            <select name="ratings" id="ratings" required>
+                <option value="one">1 Star</option>
+                <option value="two">2 Stars</option>
+                <option value="three">3 Star</option>
+                <option value="four">4 Stars</option>
+                <option value="five">5 Stars</option>
+            </select>
+        </form>
+        <ul class="bookmark-list js-bookmark-list"></ul>
+    `;
+}
+
+
+/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+// Function that will give access to users to create their own bookmark entry
+
+function newBookmarkEntryTemplate() {
+    return `  
+    <form id="js-bookmark-list-form">
+        <label for="bookmark-title-entry">Title:</label>
+        <input type="text" name="bookmark-title-entry" class="js-bookmark-title-entry" placeholder="e.g., marinara recipe"><br>
+        <label for="bookmark-link-entry">URL:</label>
+        <input type="text" name="bookmark-link-entry" class="js-bookmark-link-entry" placeholder="www.sauceboss.com"><br>
+        <label for="bookmark-desc-entry">Description:</label>
+        <input type="text" name="bookmark-desc-entry" class="js-bookmark-desc-entry" placeholder="better than momma's"><br>
+        <label for="rating">Rating:</label>
+        <input type="text" name="bookmark-rating-entry" class="js-bookmark-rating-entry" placeholder="e.g., 1 - 5"><br>
+        <button type="submit">Add +</button>
+    </form>
+  `;
+  }
+
+
+
+
+
+
+  // Function that generates newly created bookmark
 
   const generateItemElement = function (item) {
     let bookmarkDescription = `
@@ -47,21 +112,22 @@ const store = {
   };
   
   
-  // STEP 1.A.
+  //.:*~*:._.:*~*:._ STEP 1.B. _.:*~*:._.:*~*:.//
 
   const generateBookmarkItemsString = function (bookmarkList) {
     const items = bookmarkList.map((item) => generateItemElement(item));
     return items.join('');
   };
   
-  // STEP 1
+  //.:*~*:._.:*~*:._ STEP 1.A. _.:*~*:._.:*~*:.//
 
   const render = function () {
       console.log('rendering')
+      let pageLoad = generateStartTemplate();
       // Set up a copy of the store's items in a local 
       // variable 'items' that we will reassign to a new
       // version if any filtering of the list occurs.
-      let items = [...store.items];
+      let items = [...store.bookmarks];
       // If the `hideCheckedItems` property is true, 
       // then we want to reassign filteredItems to a 
       // version where ONLY items with a "checked" 
@@ -70,16 +136,17 @@ const store = {
     //   if (store.hideCheckedItems) {
     //       items = items.filter(item => !item.checked);
     //     }
-        
+
         /**
          * At this point, all filtering work has been 
          * done (or not done, if that's the current settings), 
          * so we send our 'items' into our HTML generation function
          */
-        const bookmarkListItemsString = generateBookmarkItemsString(items);
+        let bookmarkListItemsString = generateBookmarkItemsString(items);        
+        
         
         // insert that HTML into the DOM
-        $('.js-bookmark-list').html(bookmarkListItemsString);
+        $('.js-container').html(pageLoad).append(bookmarkListItemsString);
     };
     
     
