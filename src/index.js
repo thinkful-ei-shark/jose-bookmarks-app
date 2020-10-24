6e6ce91f783c01fd5f22c0c59bdf2ae19218a3e0
Joseguera <joseguera@uclaextension.edu>
2020-10-24 14:38:42 -0700

    edited index files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+//saveBookmark function:This function will check whether there are any existing bookmarks. If there are
+//imports always at top
+import $, { data } from "jquery";
+import css from './style.css';
+import api from "./api";
+import store from "./store";
+import bookmarkapp from "./bookmarkapp";
+
+//Main function to call all functions and generate to DOM
+function main() {
+  api.getItems().then((data) => {
+    data.forEach((data) => store.addBookmark(data));
+    bookmarkapp.render();
+  });
+  bookmarkapp.render();
+  bookmarkapp.bindEvenListeners();
+};
+
+$(main);
+
 /////////////////////////////////////////////////////////
 //////////////////// STORE DATABASE /////////////////////
 ///////////////////////////////////////////////////////// 
