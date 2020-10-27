93c55c0f2df8051a0af840f63cc68d9df52ae32f
Joseguera <joseguera@uclaextension.edu>
2020-10-26 20:07:55 -0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: 'index_bundle.js',
+  entry: './docs/index.js',
   output: {
     path: __dirname + '/dist',
     filename: 'index_bundle.js'
