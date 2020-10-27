de46367f701e1f7b690e8354d702de35cf46fbb8
Joseguera <joseguera@uclaextension.edu>
2020-10-27 08:40:32 -0700

    new edits

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,6 @@
 //saveBookmark function:This function will check whether there are any existing bookmarks. If there are
 //imports always at top
-import $, { data } from "jquery";
+import $ from "jquery";
 import css from './index.css';
 import api from "./api";
 import store from './store';
